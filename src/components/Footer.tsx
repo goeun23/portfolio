5ea0b7989d96464ea00c72827986b73d2ad54945
goeun23 <abgoeun3339@gmail.com>
2026-01-09 14:56:40 +0900

    260109 이력서 개편

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,4 @@
-import ContactItem from "./ContactItem";
+import ContactItem from './ContactItem';
 
 const Footer = ({
   contact,
@@ -10,14 +10,14 @@ const Footer = ({
   return (
     <footer className="flex flex-col gap-2 justify-center items-center bg-GRAY h-28 text-xs text-white dark:bg-GRAY_EXTRAHEAVY dark:text-GRAY_HEAVY">
       <div className="flex gap-1">
-        {contact.map((contact) => (
+        {contact.map(contact => (
           <ContactItem key={contact.id} {...contact}>
             {contact.name}
           </ContactItem>
         ))}
       </div>
       <div className="flex flex-col items-center">
-        <span className="whitespace-pre-wrap text-center">{`Copyright 2025. ${name} All rights reserved.`}</span>
+        <span className="whitespace-pre-wrap text-center">{`Copyright 2026. ${name} All rights reserved.`}</span>
         <a target="_blank" rel="noreferrer" href="https://github.com/sjoleee/very-simple-portfolio">
           Powered by very-simple-portfolio
         </a>
